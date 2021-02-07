ceba48dd71a65a88e96e124647f31385bdb96ec6
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-02-07 09:48:13 -0600

    Create filter_for_keyword_triples.R

---
+++ filter_for_keyword_triples.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(ngram)
+
+setwd("~/triples_in_hansard")
+
+hansard <- read_csv("hansard_c19_triples_debate_text_02022021.csv")
+
+hansard$triple <- hansard$triple %>%
+tolower()
+
+keywords <- read_csv("propertywords.csv")
+
+keywords_value <- keywords$adscription
+
+property_triples <- tibble()
+
+for(i in 1:length(keywords_value)) {
+
+  keyword <- keywords_value[i]
+
+  filtered_hansard <- hansard %>%
+    filter(str_detect(triple, keyword))
+
+property_triples <- bind_rows(property_triples, filtered_hansard) }
+
+write.csv(property_triples, "hansard_c19_debate_text_property_triples.csv")
