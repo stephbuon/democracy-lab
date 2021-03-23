6207f59eeb337f228efebc84f638e2f783b57f18
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-03-23 15:23:22 -0500

    Update filter_for_property_triples.R

--- keyword-search/filter_for_property_triples.R
+++ keyword-search/filter_for_property_triples.R
@@ -1,26 +1,27 @@
+
 library(tidyverse)
 library(ngram)
 
-setwd("/scratch/group/pract-txt-mine")
+setwd("~/triples_in_hansard")
 
 hansard <- read_csv("hansard_c19_triples_debate_text_02022021.csv")
 
 hansard$triple <- hansard$triple %>%
 tolower()
 
-keywords <- read_csv("propertywords.csv")
+keywords <- read_csv("propertywords_cleaned_for_triples.csv")
 
-keywords_value <- keywords$adscription
+keywords_value <- keywords$property_keyword
 
 property_triples <- tibble()
 
 for(i in 1:length(keywords_value)) {
-
+  
   keyword <- keywords_value[i]
-
+  
   filtered_hansard <- hansard %>%
-    filter(str_detect(triple, keyword))
+    filter(str_detect(triple, fixed(keyword, ignore_case = TRUE)))
+  
+  property_triples <- bind_rows(property_triples, filtered_hansard) }
 
-property_triples <- bind_rows(property_triples, filtered_hansard) }
-
-write.csv(property_triples, "hansard_c19_debate_text_property_triples.csv")
+write.csv(property_triples, "hansard_c19_property_triples.csv")
