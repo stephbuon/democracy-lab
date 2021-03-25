10235885ab5f37d9b87aefc5e5f7a86e17efed7c
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-03-25 15:03:48 -0500

    Update property_triples.R

--- keyword-search/property-triples/property_triples.R
+++ keyword-search/property-triples/property_triples.R
@@ -5,8 +5,8 @@ setwd("~/triples_in_hansard")
 
 hansard <- read_csv("hansard_c19_triples_debate_text_03232021.csv")
 
-hansard$triple <- hansard$triple %>%
-tolower()
+#hansard$triple <- hansard$triple %>%
+#tolower()
 
 keywords <- read_csv("propertywords_cleaned_for_triples.csv")
 
