c10098fadc9a91b79b6a30fe484dcb40376e5015
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-03-25 15:04:09 -0500

    Update future_tense_triples.R

--- keyword-search/future-tense-triples/future_tense_triples.R
+++ keyword-search/future-tense-triples/future_tense_triples.R
@@ -3,6 +3,6 @@ library(tidyverse)
 hansard <- read_csv("hansard_c19_triples_debate_text_03232021.csv")
 
 filtered_hansard <- hansard %>%
-filter(str_detect(triple, fixed("-will-", ignore_case = TRUE)))
+filter(str_detect(triple, regex("-will-", ignore_case = TRUE)))
 
 write.csv(filtered_hansard, "hansard_c19_future_tense_triples_debate_text_03232021.csv")
