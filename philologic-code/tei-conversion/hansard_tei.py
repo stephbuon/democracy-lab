a498b409095c266f642b7998599d5c441d40e9aa
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-03-29 12:52:08 -0500

    Update hansard_tei.py

--- philologic-code/tei-conversion/hansard_tei.py
+++ philologic-code/tei-conversion/hansard_tei.py
@@ -2,11 +2,14 @@
 
 import pandas as pd
 import trafilatura
+import os
 
 # f = '/scratch/group/pract-txt-mine/data_sets/hansard/hansard_20191119.tsv' # for all of Hansard. No year column
 
 f = '~/hansard_c19_04152021.tsv' # for c19 Hansard. With year column 
 
 hansard = pd.read_csv(f, sep='\t')
+
+os.chdir('/scratch/group/pract-txt-mine')
 
 hansard.to_html('hansard_c19_12192019.html', index=False)
