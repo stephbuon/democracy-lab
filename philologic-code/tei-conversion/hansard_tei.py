b8a04f812ebd823b3d9830fec6ce0a9585e644b2
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-03-30 18:00:10 -0500

    Add files via upload

--- philologic-code/tei-conversion/hansard_tei.py
+++ philologic-code/tei-conversion/hansard_tei.py
@@ -1,17 +1,20 @@
-#!pip install --user trafilatura
-
 import pandas as pd
 import trafilatura
 import os
 
 # f = '/scratch/group/pract-txt-mine/data_sets/hansard/hansard_20191119.tsv' # for all of Hansard. No year column
 
-f = '~/hansard_c19_04152021.tsv' # for c19 Hansard. With year column 
+directory = '/scratch/group/pract-txt-mine'
+basename = 'hansard_c19_04152021'
 
+os.chdir(directory)
+f = f'{basename}.tsv' # for c19 Hansard. With year column 
+print('Reading TSV...')
 hansard = pd.read_csv(f, sep='\t')
-
-os.chdir('/scratch/group/pract-txt-mine')
-
-hansard = hansard.to_html('hansard_c19_12192019.html', index=False)
-
+print('Converting to HTML...')
+hansard = hansard.to_html(f'{basename}.html', index=False)
+print('Converting to TEI-XML...')
 hansard_tei = trafilatura.extract(hansard, tei_output=True, tei_validation=True)
+print('Exporting TEI-XML to file...')
+with open(f'{basename}.tei', 'w+') as f:
+    f.write(hansard_tei)
