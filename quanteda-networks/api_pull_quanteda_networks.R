4352460aaf318fb5d1b6923a424d7a480a9ee016
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-04-07 10:29:52 -0500

    Update api_pull_quanteda_networks.R

--- quanteda-networks/api_pull_quanteda_networks.R
+++ quanteda-networks/api_pull_quanteda_networks.R
@@ -1,3 +1,6 @@
 library(boxr)
 
-box_auth(client_id = "your_client_id", client_secret = "your_client_secret")
+box_auth(client_id = "uvyxxh3ldgiyv8s52xb02jud6vlaxfkt", 
+         client_secret = "EAyZpKYM8adgK6Y6djtx9lPtemQdXcSr")
+
+box_search("concerns.csv")
