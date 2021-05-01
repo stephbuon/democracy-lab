ebec939026de9eab4c1cc329d8ac274d19ece7f1
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-05-01 15:46:05 -0500

    Create triples_list.R

---
+++ named-entities/temporal-events-tables/triples_list.R
@@ -0,0 +1,89 @@
+triples_for_1900 <- c("it-be-in-1794", "country-be-in-jeopardy", "word-speak-against-king", #  french rev: no other options
+                      "history-mislead-by-error", "we-have-war", "exaggeration-indulge-during-war", # boer war: history-begin-in-1795, officer-reemploye-during-emergency, officer-hold-rank
+                      "tenant-do-repair", "tenant-hold-farm", "tenant-have-landlord", # act of 1881: no other options  
+                      "debt-stand-in-1866", "he-propose-taxation", "we-march-through-london") # crimean war: battalion-volunteer-for-service, we-think-with-complacency, minister-yield-allegiance
+
+triples_for_1890 <- c("which-cost-million", "we-liberate-portion", "i-ask-for-war", # crimean war: no other options
+                      "regulation-regard-disease", "regulation-introduce-into-perak", "acts-be-in-force", # contagious disease acts: police-engage-in-cotriples_countection, police-live-on-ship, regulation-regard-disease, operation-have-effect
+                      "tenant-seek-relief", "tenant-deprive-of-benefit", "privilege-confer-upon-tenant", # 1881: state-vote-without-voice, lessee-apply-for-relief, lessor-enforce-right, lessor-grant-relief, grievance-be-of-consequence
+                      "state-vote-without-voice", "million-squander-on-war", "india-go-through-experience", # afghan war: state-vote-without-voice, million-squander-on-frontier, parliament-vote-in-1880, india-go-through-much, it-go-in-famine
+                      "tenant-pay-rent", "tenant-refund-by-landlord", "tenant-refund-difference", # land act: tenant-lodge-for-year, government-consider-proposal
+                      "tariff-come-into-force", "end-put-to-war", "treaty-negotiate-in-1882", # treaty: treaty-contain-stipulation, britain-defend-acheenese, britain-defend-from-aggression
+                      "soldier-wound-in-action", "which-lame-him", "which-lame-for-life") # indian mutany: money-await-claimant
+
+triples_for_1880 <- c("drunketriples_countess-decrease-in-city", "torpedo-bring-to-country", "drunketriples_countess-decrease-from-1871", # 1881: tunis-be-independent, government-arrange-for-dissolution, parliament-promise-to-tenant, act-fix-rent
+                      "hydrographer-make-investigation", "increase-pay-by-tenant", "landlord-pay-for-disturbance", # 1870: property-have-protection, tenant-have-claim, landlord-acquire-right, government-exercise-surveillance
+                      "glander-prevail-among-horse", "member-confound-with-russia", "vessel-arrive-at-suez", #1882: failure-necessitate-action, land-be-at-unlet
+                      #"arrest-increase-during-period", "arreset-increase-in-county", "intemperance-decrease-in-district", # I cant figure out which event/ date these belong to
+                      "they-complete-curlew", "party-have-right", "overseer-misdescribe-nature", # 1885: we-avoid-danger, majesty-despatch-with-hope, lord-criticize-provision
+                      "france-be-independent", "tunis-be-independent", "engagement-take-by-country", # treaty: treaty-be-insufferable, france-have-treaty, treaty-be-disappointing
+                      "war-labour-in-gloom", "order-send-for-army", "order-send-for-gun", # crimean war: germany-be-concern, beaconsfield-increase-force, rent-make-by-farmer, country-help-turkey
+                      "england-make-contribution", "500-grant-from-fund", "500-grant-reward", # afghan war: contribution-make-by-government, year-darken-by-war, government-be-ashamed, i-come-to-war
+                      "bill-propose-repeal", "disease-rise-between-year", "disease-rise-in-number", # contagious diseases acts: officer-attend-meeting, ward-become-large, officer-command-regiment
+                      "expenditure-incur-in-year", "he-ask-for-war", "officer-reward-for-service", # zulu war: chiefs-cede-to-boers, war-break-in-spring, he-command-troop, they-engage-in-war
+                      "they-engage-in-war", "government-protect-ally", "government-protect-from-ruin", # transvaal war: boers-make-war, difficulty-appear-from-papers
+                      "operation-be-effectual", "bill-propose-repeal", "disease-rise-in-number", # contagious disease acts: state-be-terrible, young-be-rampant, ward-become-large
+                      "mutiny-come-after-war", "concession-grow-gloomy", "concession-have-trouble", # american war: no other options
+                      "landlord-remove-tenant", "tenant-entitle-to-compensation", "tenant-receive-notice", # amendment: bill-read-mischievous, government-feel-bind, he-reject-amendment, system-oppose-amendment
+                      "catholics-attend-service", "prayer-read-for-catholics", "troopship-have-chaplain") # service of protestant chaplains: chaplain-be-available, prayer-read-by-officer, troopship-have-chaplain
+
+triples_for_1870 <- c("woman-take-into-custody","woman-take-without-warrant", "woman-detain-in-hospital", # act: they-complain-of-tramway, person-keep-shop, parliament-pass-act, construction-adopt-by-public
+                      "they-have-war", "sum-cover-expenditure", "bonds-make-total", # zulu war: they-be-anxious, troop-engage-during-day, gentleman-be-confident, colonists-be-incline
+                      "khan-be-loyal", "india-contribute-for-expenditure", "government-plunge-into-war", # afghan war: i-remember-war, he-be-surprised, war-bring-to-close, war-involve-charge
+                      "war-increase-debt", "sum-be-great", "war-commence-in-1854", # crimean war: gentleman-reduce-cavalry, commission-appoint-in-1857, difficulty-lead-to-promulgation, war-follow-period
+                      "woman-detain-in-hospital", "woman-subject-by-order", "woman-subject-to-examination", # contagious disease acts: man-have-idea, woman-put-in-position, paris-have-population
+                      "woman-detain-in-hospital", "penalty-impose-in-england", "gentleman-direct-against-act", # act of 1869: woman-subject-to-examination 
+                      "debate-occur-in-1775", "objection-raise-in-1781", "pauperism-increase-between-1859", # american war: troop-occupy-position, it-advise-sovereign, it-be-beyond-duty
+                      "cruelty-perpetrate-for-year", "cruelty-perpetrate-upon-them", "rayahs-enjoy-protection", # war of independence: no other options
+                      "injustice-commit-under-cover", "reply-be-inconclusive", "which-force-conviction", # vietriples_counta exhibition: no other options
+                      "treaty-conclude-in-1860", "which-organize-during-famine", "minister-extend-power", # french treaty: no other options 
+                      "dissatisfaction-prevail-in-china", "sultan-atriples_countounce-at-council", "china-open-port", # treaty: chancellor-include-in-budget, government-accede-to-suggestion, opium-admit-into-japan, nation-be-prepared
+                      "indemnity-pay-by-turkey", "indemnity-pay-to-russia", "integrity-guarantee-by-powers") #, # treaty of san stefano: no other options
+# "discontent-be-strong", "country-ruin-by-war", "statesman-foresee-downfall", # french revolution: find the correct decade 
+# "parliament-have-courage", "presbytery-appoint-minister", "house-disregard-restriction") # revolution: find the correct decade 
+
+triples_for_1860 <- c("colony-separate-from-england", "i-examine-before-revolution", "she-tax-them", # american revolution 
+                      "artist-expose-themselves", "salviati-come-to-england", "artist-have-copyright", # great exhibition 
+                      "revolution-stimulate-feeling", "centralization-contribute-to-destruction", "french-take-possession", # french revolution
+                      "support-be-prepared", "you-disestablish-church", "lords-guarantee-between-england", # protestant revolution
+                      "declaration-adopt-in-1856", "poles-maintain-contest", "militia-supply-man", # crimean war 
+                      "paper-establish-throughout-italy", "paper-oppose-progress", "revolution-take-place", # revolution 
+                      "dissenters-attract-by-declaration", "jesuitism-insinuate-into-affair", "power-become-supreme") # rev of 1688
+
+triples_for_1850 <- c("vessel-carry-passenger", "vessel-bind-for-havatriples_countah", "110-perish-on-voyage",
+                      "bourbons-restore-to-throne", "neutrality-conclude-between-denmark", "restitution-make-for-property",
+                      "manufacture-be-on-increase", "object-exclude-catholics", "person-be-anxious",
+                      "constitution-become-protestant", "act-pass-after-revolution", "they-suffer-under-enactment",
+                      "turks-cross-danube", "turks-declare-war", "one-inspire-by-fanaticism")
+
+triples_for_1840 <- c("revolution-be-in-1792", "revolution-be-at-height", "religion-suffer-in-revolution",
+                      #"performance-allow-during-lent", "performance-tolerate-during-lent", "duty-be-severe",
+                      "law-enact-in-country", "law-enact-in-1688", "system-endure-since-1688-he",
+                      "england-establish-faith", "england-shake-domination", "prophet-speak-of-day",
+                      "crop-become-unproductive", "prospect-offer-in-country", "he-extend-to-cuba",
+                      "americans-have-right", "spain-have-right", "england-watch-over-welfare")
+
+triples_for_1830 <- c("papists-invade-in-country", "atrocity-perpetrate-under-excitement", "countryman-raise-impostor",
+                      "dissolution-take-before-revolution", "spain-disgrace-by-atrocity", "event-inflame-people",
+                      #"prince-pray-for-relaxation", "he-approve-of-relaxation", "it-observe-on-friday",
+                      "christianity-abjure-in-1793-in", "slave-send-for-emancipation", "france-reap-from-assembly",
+                      "population-enjoy-happiness", "young-be-in-error", "country-border-on-anarchy",
+                      "change-take-place", "constitution-be-different", "rebellion-produce-feeling",
+                      "domingo-lose-possession", "domingo-sever-from-france", "she-exercise-sovereignty",
+                      "buonaparte-contemplate-slavery", "plantation-cultivate-by-labourer", "plantation-cultivate-in-1801")
+
+triples_for_1820 <- c("pope-excommunicate-council", "scarcity-prevail-over-country", "taxation-produce-revolution",
+                      "revolution-take-place", "they-acknowledge-him", "they-acknowledge-for-king",
+                      "law-pass-against-bribery", "ancestor-consider-at-revolution", "history-date-from-reformation",
+                      "war-excite-uneasiness", "government-seize-property", "government-seize-in-violation",
+                      "sugar-have-regulation", "sugar-smuggle-into-part", "petitioner-grant-emancipation")
+
+triples_for_1810 <- c("distress-reach-height", "troop-maintain-by-guinea", "crime-augment-in-war",
+                      "revolution-spread-throughout-world", "revolution-be-in-vigour", "independence-swallow-in-gulf",
+                      "sentiment-take-place", "sentiment-take-in-dublin", "sentiment-express-in-parliament",
+                      "it-assail-church", "country-deprive-of-trade", "ireland-recover-trade")
+
+triples_for_1800 <- c("baird-sail-on-9th", "baird-receive-intelligence", "we-send-for-army",
+                      "fate-deter-us", "critic-estimate-production", "critic-be-unreasonable",
+                      "general-order-delinquent", "court-exercise-power", "they-labour-in-consequence",
+                      "that-desolate-world", "which-be-for-year") 
