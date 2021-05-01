bf30d472e06ca14613353b95414fac77b5801c66
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-05-01 15:41:40 -0500

    Update events_list.R

--- named-entities/temporal-events-tables/events_list.R
+++ named-entities/temporal-events-tables/events_list.R
@@ -1,85 +1,85 @@
 events_for_1900 <- c("french revolution",
-                       "boer war",
-                       "act", 
-                       "crimean war")
-  
-  events_for_1890 <- c("transvaal war",
-                       "crimean war",
-                       "contagious diseases acts",
-                       "indian mutiny",
-                       "crimean war",
-                       "1881", #tenant seek relief 
-                       "treaty", # treaty enter between france
-                       "afghan war", # war pay by india 
-                       "land act") # tenant seek relief, tenant-labour-under-injustice
-  
-  events_for_1880 <- c("1870",
-                       "1878",
-                       "1881", 
-                       "1882", 
-                       "1885",
-                       "amendment", # landlord-remove-tenant, tenant-entitle-to-compensation, tenant-receive-notice
-                       "afghan war",
-                       "american war",
-                       "contagious diseases acts",
-                       "crimean war",
-                       "service of protestant chaplains",
-                       "transvaal war",
-                       "treaty", 
-                       "zulu war")
-  
-  events_for_1870 <- c("zulu war",
-                       "afghan war",
-                       "crimean war",
-                       "contagious diseases acts",
-                       "act of 1869",
-                       "act",
-                       "american war",
-                       "war of independence",
-                       "vietriples_counta exhibition",
-                       "french treaty",
-                       "indian mutiny",
-                       "treaty",
-                       "treaty of san stefano")
-  
-  events_for_1860 <- c("french revolution",
-                       "crimean war",
-                       "revolution",
-                       "russian war",
-                       "great exhibition")
-  
-  events_for_1850 <- c("french revolution",
-                       "vietriples_counta conference",
-                       "revolution",
-                       "chinese passengers act")
-  
-  events_for_1840 <- c("french revolution",
-                       #"lent",
-                       "st. domingo",
-                       "revolution",
-                       "revolution of 1688",
-                       "treaty")
-  
-  events_for_1830 <- c("french revolution", 
-                       "national convention",
-                       "revolution",
-                       #"lent",
-                       "st. domingo",
-                       "1807", 
-                       "st. domingo and guadaloupe")
-  
-  events_for_1820 <- c("french revolution",
-                       "spanish revolution",
-                       "revolution",
-                       "st. domingo", 
-                       "french war")
-  
-  events_for_1810 <- c("french revolution", 
-                       "crimean war",
-                       "american war",
-                       "revolution of 1688", 
-                       "grand orange lodge") 
-  
-  events_for_1800 <- c("st. domingo",
-                       "revolution",
-                       "convention of cintra") }
+                     "boer war",
+                     "act", 
+                     "crimean war")
+
+events_for_1890 <- c("transvaal war",
+                     "crimean war",
+                     "contagious diseases acts",
+                     "indian mutiny",
+                     "crimean war",
+                     "1881", #tenant seek relief 
+                     "treaty", # treaty enter between france
+                     "afghan war", # war pay by india 
+                     "land act") # tenant seek relief, tenant-labour-under-injustice
+
+events_for_1880 <- c("1870",
+                     "1878",
+                     "1881", 
+                     "1882", 
+                     "1885",
+                     "amendment", # landlord-remove-tenant, tenant-entitle-to-compensation, tenant-receive-notice
+                     "afghan war",
+                     "american war",
+                     "contagious diseases acts",
+                     "crimean war",
+                     "service of protestant chaplains",
+                     "transvaal war",
+                     "treaty", 
+                     "zulu war")
+
+events_for_1870 <- c("zulu war",
+                     "afghan war",
+                     "crimean war",
+                     "contagious diseases acts",
+                     "act of 1869",
+                     "act",
+                     "american war",
+                     "war of independence",
+                     "vietriples_counta exhibition",
+                     "french treaty",
+                     "indian mutiny",
+                     "treaty",
+                     "treaty of san stefano")
+
+events_for_1860 <- c("french revolution",
+                     "crimean war",
+                     "revolution",
+                     "russian war",
+                     "great exhibition")
+
+events_for_1850 <- c("french revolution",
+                     "vietriples_counta conference",
+                     "revolution",
+                     "chinese passengers act")
+
+events_for_1840 <- c("french revolution",
+                     #"lent",
+                     "st. domingo",
+                     "revolution",
+                     "revolution of 1688",
+                     "treaty")
+
+events_for_1830 <- c("french revolution", 
+                     "national convention",
+                     "revolution",
+                     #"lent",
+                     "st. domingo",
+                     "1807", 
+                     "st. domingo and guadaloupe")
+
+events_for_1820 <- c("french revolution",
+                     "spanish revolution",
+                     "revolution",
+                     "st. domingo", 
+                     "french war")
+
+events_for_1810 <- c("french revolution", 
+                     "crimean war",
+                     "american war",
+                     "revolution of 1688", 
+                     "grand orange lodge") 
+
+events_for_1800 <- c("st. domingo",
+                     "revolution",
+                     "convention of cintra") 
