4af4d87a9a7641b4d13ef32572146f6ab5948373
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-05-01 15:58:46 -0500

    Update debug_entity_count.R

--- named-entities/temporal-events-tables/debug_entity_count.R
+++ named-entities/temporal-events-tables/debug_entity_count.R
@@ -1,35 +1,26 @@
-# verify that entity counts are correct
+## count entity debugging script
 
 library(tidyverse)
 
 keyword <- "crimean war"
+interval <- 10
 
-return_entity_count <- function(variable, kw) {
+hansard_named_temporal_events_triples <- read_csv("hansard_named_temporal_events_triples.csv") %>%
+  mutate(decade = year - year %% interval)
+
+return_entity_count <- function(variable, pattern) {
   out <- variable %>%
-    filter(str_detect(entity, regex(kw, ignore_case = TRUE)) %>%
+    filter(str_detect(entity, regex(pattern, ignore_case = TRUE)) %>%
     group_by(entity) %>%
     add_count(entity) %>%
+    select(entity, n) %>%
     ungroup()
   return(out) }
 
-count_1 <- hansard_named_events %>%
-  filter(str_detect(entity, regex(keyword, ignore_case = TRUE))) %>%
-  group_by(entity) %>%
-  add_count(entity) %>%
-  ungroup()
 
-count_2 <- hansard_named_times %>%
-  filter(str_detect(entity, regex(keyword, ignore_case = TRUE))) %>%
-  group_by(entity) %>%
-  add_count(entity) %>%
-  ungroup()
+count_1 <- return_entity_count(hansard_named_events, keyword)
 
-interval <- 10
+count_2 <- return_entity_count(hansard_named_times, keyword)
 
-count_3 <- read_csv("hansard_named_temporal_events_triples.csv") %>%
-  mutate(decade = year - year %% interval)
-
-
-return_entity_count
-
+count_3 <- return_entity_count(hansard_named_temporal_triples, keyword)
 
