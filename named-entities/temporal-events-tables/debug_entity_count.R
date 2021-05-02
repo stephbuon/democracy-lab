04071f6be16286107bdad7b60551dcf25df51435
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-05-02 10:17:11 -0500

    Update debug_entity_count.R

--- named-entities/temporal-events-tables/debug_entity_count.R
+++ named-entities/temporal-events-tables/debug_entity_count.R
@@ -1,14 +1,11 @@
-# count entity debugging script
-
 library(tidyverse)
 
 keyword <- "crimean war"
-interval <- 10
 
-#hansard_named_temporal_events_triples <- read_csv("hansard_named_temporal_events_triples.csv") %>%
-
-#hansard_named_temporal_events_triples
-#  mutate(decade = year - year %% interval)
+if(!exists("hansard_named_temporal_events")) {
+  interval <- 10
+  hansard_named_temporal_events <- read_csv("hansard_named_temporal_events.csv") %>%
+    mutate(decade = year - year %% interval) }
 
 return_entity_count <- function(variable, pattern, filter_decade = 0) {
   if(filter_decade == TRUE) {
@@ -29,5 +26,6 @@ count_1 <- return_entity_count(hansard_named_events, keyword)
 
 count_2 <- return_entity_count(hansard_named_times, keyword)
 
-count_3 <- return_entity_count(entites_triples_w_event_count, keyword, filter_decade = FALSE)
+count_3 <- return_entity_count(entity_count, keyword, filter_decade = FALSE)
+
 
