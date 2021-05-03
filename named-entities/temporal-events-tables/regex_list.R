08bd9144cfc56b24d5a53e9bc975106193b4e9ae
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-05-02 19:15:18 -0500

    Create regex_list.R

---
+++ named-entities/temporal-events-tables/regex_list.R
@@ -0,0 +1,88 @@
+regex_for_1900 <- c("french revolution",
+                     "boer war",
+                     "^act$", 
+                     "crimean war")
+
+regex_for_1890 <- c("transvaal war",
+                     "crimean war",
+                     "contagious diseases acts",
+                     "indian mutiny",
+                     "crimean war",
+                     "1881", #tenant seek relief 
+                     "^treaty$", # treaty enter between france
+                     "afghan war", # war pay by india 
+                     "land act") # tenant seek relief, tenant-labour-under-injustice
+
+regex_for_1880 <- c("1870",
+                     "1878",
+                     "1881", 
+                     "1882", 
+                     "1885",
+                     "^amendment$", # landlord-remove-tenant, tenant-entitle-to-compensation, tenant-receive-notice
+                     "afghan war",
+                     "american war",
+                     "contagious diseases acts",
+                     "crimean war",
+                     "service of protestant chaplains",
+                     "transvaal war",
+                     "^treaty$", 
+                     "zulu war")
+
+regex_for_1870 <- c("zulu war",
+                     "afghan war",
+                     "crimean war",
+                     "contagious diseases acts",
+                     "act of 1869",
+                     "^act$",
+                     "american war",
+                     "war of independence",
+                     "great exhibition",
+                     "french treaty",
+                     "indian mutiny",
+                     "^treaty$",
+                     "treaty of san stefano")
+
+regex_for_1860 <- c("french revolution",
+                     "protestant revolution",
+                     "american revolution",
+                     "revolution of 1866",
+                     "crimean war",
+                     "^revolution$",
+                     "russian war",
+                     "great exhibition")
+
+regex_for_1850 <- c("french revolution",
+                     "vietriples_counta conference",
+                     "^revolution$",
+                     "chinese passengers act")
+
+regex_for_1840 <- c("french revolution",
+                     #"lent",
+                     "st. domingo",
+                     "^revolution$",
+                     "revolution of 1688",
+                     "^treaty$")
+
+regex_for_1830 <- c("french revolution", 
+                     "national convention",
+                     "revolution",
+                     #"lent",
+                     "^st. domingo$",
+                     "1807", 
+                     "st. domingo and guadaloupe")
+
+regex_for_1820 <- c("french revolution",
+                     "spanish revolution",
+                     "^revolution$",
+                     "st. domingo", 
+                     "french war")
+
+regex_for_1810 <- c("french revolution", 
+                     "crimean war",
+                     "american war",
+                     "revolution of 1688", 
+                     "grand orange lodge") 
+
+regex_for_1800 <- c("st. domingo",
+                     "^revolution$",
+                     "convention of cintra") 
