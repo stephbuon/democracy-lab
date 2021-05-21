667e5dc02daed51ab8b12c6293bbd5d03c66475d
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-05-21 14:57:51 -0500

    Update visualize_temporal_events_count.R

--- named-entities/temporal-events-counts/visualize_temporal_events_count.R
+++ named-entities/temporal-events-counts/visualize_temporal_events_count.R
@@ -8,7 +8,7 @@ countedevents <- read_csv("entity_count.csv")
 subset <- countedevents %>% 
   filter(scholar_assigned_date > 1770) 
 
-ggplot(subset, aes(x = century, 
+ggplot(subset, aes(x = decade, 
                    y = scholar_assigned_date, 
                    color = n)) + #, 
                    #label = paste0(entity, ' (', scholar_assigned_date, ')') )) + 
@@ -18,10 +18,11 @@ ggplot(subset, aes(x = century,
                     group_by(entity) %>% 
                     arrange(desc(n)) %>% 
                     slice(2), 
-                  aes(color = 1, x = century + 0.03, 
-                      label = paste0(entity),
-                      #label = paste0(entity, ' (', scholar_assigned_date, ')'), 
-                      size = n*5), #size = rel(4), 
+                  aes(color = 1, x = decade + 0.03, 
+                      #label = paste0(entity),
+                      label = paste0(entity, ' (', scholar_assigned_date, ')'), 
+                      size = n*5),
+                  size = rel(4), 
                   hjust=0) +
   labs(x = "timeline", y = "mentions per year") +
   ggtitle("Events Mentioned by Name in Parliament")
