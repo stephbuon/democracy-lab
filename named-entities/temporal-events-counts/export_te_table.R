c6b47c0a7fb116d31ae9f4346943b87b63f8d1dd
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-05-24 09:08:15 -0500

    Update export_te_table.R

--- named-entities/temporal-events-counts/export_te_table.R
+++ named-entities/temporal-events-counts/export_te_table.R
@@ -3,7 +3,6 @@ library(tidyverse)
 library(viridis)
 
 counted_events <- read_csv("~/entity_count_05242021.csv")
- put correct file path 
 
 out <- counted_events %>%
   group_by(entity, scholar_assigned_date) %>%
@@ -11,5 +10,4 @@ out <- counted_events %>%
 
 # contents can be copied/pasted into a word doc
 # inside the word doc, the user can highlight the contents and go to table -> convert -> convert text to table 
-write.table(out, paste0(file = "entities_count_table.txt"), sep = ",", quote = FALSE, row.names = F)
-
+write.table(out, paste0(file = "entities_count_table_05242021.txt"), sep = ",", quote = FALSE, row.names = F)
