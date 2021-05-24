f2653d26d75ee0e0efd29e95002320bb756b772d
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-05-24 08:09:44 -0500

    Update export_te_table.R

--- named-entities/temporal-events-counts/export_te_table.R
+++ named-entities/temporal-events-counts/export_te_table.R
@@ -1,18 +1,13 @@
-### graphing
 library(ggrepel)
 library(tidyverse)
 library(viridis)
 
-#countedevents <- read_csv("~/entity_count.csv")
-# put correct file path 
+counted_events <- read_csv("~/entity_count_05242021.csv")
+ put correct file path 
 
-#out <- countedevents %>%
-#  select(-decade)
-
-out <- out %>%
+out <- counted_events %>%
   group_by(entity, scholar_assigned_date) %>%
   summarize(total = sum(occurances))
-
 
 # contents can be copied/pasted into a word doc
 # inside the word doc, the user can highlight the contents and go to table -> convert -> convert text to table 
