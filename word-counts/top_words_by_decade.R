c5bf0e3e08f3043ffbe27b3ba83d6d5f53c006a4
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-05-28 15:40:23 -0500

    Update top_words_by_decade.R

--- word-counts/top_words_by_decade.R
+++ word-counts/top_words_by_decade.R
@@ -1,29 +1,38 @@
 library("tidyverse")
 library("lubridate")
 library("scales")
+library("tidytext")
 
-tidyhans <- read_csv("/scratch/group/pract-txt-mine/tokenized_hansard.csv")
+hansard <- read_csv("/scratch/group/pract-txt-mine/tokenized_hansard.csv")
 
-tidyhans <- tidyhans %>%
-  mutate(year = year(as.Date(tidyhans$speechdate)))
+hansard <- hansard %>%
+  rename(word = ngrams)
 
-tidyhans <- tidyhans %>%
+hansard <- hansard %>%
+  anti_join(get_stopwords())
+
+hansard <- hansard %>%
+  mutate(year = year(as.Date(hansard$speechdate)))
+
+hansard <- hansard %>%
   mutate(decade = year - year %% 10) %>%
-  group_by(decade, ngrams) %>%
+  group_by(decade, word) %>%
   summarize(n = n()) %>%
   top_n(5) %>%
-ungroup()
+  ungroup()
 
-tidyhans %>%
-  ggplot(aes(x = ngrams, 
-             y = n)) +
-  geom_bar() +
-  facet_wrap(~decade) +
-  ggtitle("Top Words by Decade") +
-  labs(x = "raw count", y = "word") +
-  theme(axis.text.x = element_text(angle = 90)) +
-  scale_x_continuous(labels = comma)
+ggplot(data = hansard) +
+  geom_col(aes(x = reorder_within(word, n, decade), 
+               y = n),
+           fill = "steel blue") +
+  labs(title = "Top Words by Decade",
+       subtitle = "From 19th-century British Parliamentary Debate Text",
+       x = "word",
+       y = "raw count") +
+  scale_x_reordered() +
+  facet_wrap(~ decade, scales = "free") + 
+  coord_flip() 
+
 
 setwd("~/")
 ggsave("top_words_by_decade.pdf", h = 4, w = 6, units = "in", dpi = 500)
-
