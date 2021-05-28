3fd94b6ba981b196ac5d8a4ab48c75674269e40b
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-05-28 11:59:36 -0500

    Update top_words_by_decade.R

--- word-counts/top_words_by_decade.R
+++ word-counts/top_words_by_decade.R
@@ -1,5 +1,6 @@
 library("tidyverse")
 library("lubridate")
+library("scales")
 
 tidyhans <- read_csv("/scratch/group/pract-txt-mine/tokenized_hansard.csv")
 
@@ -8,19 +9,20 @@ tidyhans <- tidyhans %>%
 
 tidyhans <- tidyhans %>%
   mutate(decade = year - year %% 10) %>%
-  group_by(decade) %>%
+  group_by(decade, ngrams) %>%
   summarize(n = n()) %>%
-  top_n(5)
+  top_n(5) %>%
+ungroup()
 
 tidyhans %>%
-  ggplot(aes(x = word, 
+  ggplot(aes(x = ngrams, 
              y = n)) +
   geom_bar() +
   facet_wrap(~decade) +
   ggtitle("Top Words by Decade") +
   labs(x = "raw count", y = "word") +
-  theme(axis.text.x = element_text(angle = 90)) #+
-  #scale_x_continuous(labels = comma)
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_x_continuous(labels = comma)
 
 setwd("~/")
 ggsave("top_words_by_decade.pdf", h = 4, w = 6, units = "in", dpi = 500)
