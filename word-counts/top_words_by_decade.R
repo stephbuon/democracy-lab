7cb3b3517813a6a6a803c1d206b39b1d8c2cfc5f
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-05-27 21:34:11 -0500

    Create top_words_by_decade.R

---
+++ word-counts/top_words_by_decade.R
@@ -0,0 +1,25 @@
+library("tidyverse")
+
+tidyhans <- read_csv("/scratch/group/pract-txt-mine/tokenized_hansard.csv")
+
+tidyhans <- tidyhans %>%
+  mutate(year = year(as.Date(hansard_sample$speechdate)))
+
+top_words_per_decade <- tidyhans %>%
+  mutate(decade = year - year %% 10) %>%
+  group_by(decade) %>%
+  summarize(n = n()) %>%
+  top_n(5)
+
+top_words_per_decade %>%
+  ggplot(aes(x = word, 
+             y = n)) +
+  geom_bar() +
+  facet_wrap(~decade) +
+  ggtitle("Top Words by Decade") +
+  labs(x = "raw count", y = "word") +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_x_continuous(labels = comma)
+
+setwd(~/)
+ggsave("top_words_by_decade.pdf", h = 4, w = 6, units = "in", dpi = 500)
