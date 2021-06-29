30cd7332a5b6120763a27926d39a85bb7acfdb87
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-06-29 13:00:17 -0500

    Create gensim_functions.py

---
+++ util/gensim_functions.py
@@ -0,0 +1,53 @@
+import os
+import gensim
+import pandas as pd
+
+
+def export_gensim_models(dir_path, n_cores):
+    file_names = []
+    cycle = 0
+    
+    for fname in os.listdir(dir_path):
+        file_names.append(fname)
+        
+    for fname in file_names:
+        cycle = cycle + 1
+        
+        imported_data = pd.read_csv(dir_path + fname)
+        
+        sentences_df = parallelize_operation(imported_data, str_split_sentences, n)
+        
+        sentences_df['sentence'] = sentences_df['sentence'].str.split()
+        
+        period_model = gensim.models.Word2Vec(sentences = sentences_df['sentence'],
+                                             workers = n_cores, 
+                                             min_count = 20, # remove words stated less than 20 times
+                                             size = 100) # size of neuralnet layers; default is 100 - go higher for larger corpora 
+        
+        extention_position = fname.index('.')
+        fname = fname[0:extention_position]
+                
+        if cycle == 1:
+            congress_model = period_model
+        else:
+            congress_model.build_vocab(sentences_df['sentence'], update = True)
+            congress_model.train(sentences_df['sentence'], total_examples = period_model.corpus_count, epochs = period_model.epochs)
+        
+        save_name = os.path.join(dir_path, fname)
+        
+        congress_model.save(save_name + '_model')
+
+
+def keyword_context(dir_path, keyword):
+    keyword_context = []
+    
+    for fname in os.listdir(dir_path):
+        if '_model' in fname:
+            congress_model = gensim.models.Word2Vec.load(dir_path + fname)
+            if keyword in congress_model.wv.vocab:
+                keyword_context_period = congress_model.wv.most_similar(keyword, topn = 1000)
+                keyword_context.append(keyword_context_period)
+            else:
+                continue
+                
+    return keyword_context
