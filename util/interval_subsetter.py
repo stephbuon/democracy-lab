d029c13444954182de8a2c7c9b78b68319313946
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-06-29 16:51:02 -0500

    Update interval_subsetter.py

--- util/interval_subsetter.py
+++ util/interval_subsetter.py
@@ -18,9 +18,10 @@ def interval_subset(data, col_name, start, end, intv, fname):
         start = start + intv
         subset = data[(data[col_name] >= start - intv) & (data[col_name] <= start - 1)]
         
-        descr = str(subset[col_name].iloc[0])
-        descr_2 = str(subset[col_name]. iloc[-1])
-        
-        file_name = fname + descr + "_" + descr_2
-        save_name = os.path.join(target_folder, file_name)
-        subset.to_csv(save_name + ".csv", index = False)
+        if not subset.empty: # just added this line 
+            descr = str(subset[col_name].iloc[0])
+            descr_2 = str(subset[col_name]. iloc[-1])
+            
+            file_name = fname + descr + "_" + descr_2
+            save_name = os.path.join(target_folder, file_name)
+            subset.to_csv(save_name + ".csv", index = False)
