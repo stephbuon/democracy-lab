04a7084d2232f5f885bb97b1f96185f211fbbf17
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-06-30 07:54:41 -0500

    Update subset_c20_hansard.R

--- subset-hansard/subset_c20_hansard.R
+++ subset-hansard/subset_c20_hansard.R
@@ -11,5 +11,8 @@ hansard <- hansard %>%
   filter(year(speechdate) > firstyear) %>%
   filter(year(speechdate) < lastyear)
 
+hansard <- hansard %>%
+  mutate(year = year(as.Date(hansard$speechdate))
+
 dir <- setwd("~/")
 write_csv(hansard, file.path(dir, "hansard_c20_12192019.csv"))
