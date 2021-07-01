8ab7356ebe9ded78cae53e54b543fa63386f37a7
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-07-01 09:55:05 -0500

    Update and rename str_split_sentences.py to str_functions.py

--- util/pyfunctions/str_functions.py
+++ util/pyfunctions/str_functions.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import re
 
-def str_split_sentences(df):
+def str_split_df_sentences(df, remove_punctuation, remove_punctiation == True):
     split_rule = r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s"
     
     df['speech'] = df['speech'].apply(lambda x: re.split(split_rule, x))
@@ -9,5 +9,8 @@ def str_split_sentences(df):
     df = df.explode('speech')
     df.reset_index()
     df.rename(columns = {'speech': 'sentence'}, inplace = True)
+    
+    if remove_punctuation == True:
+        df['sentence'] = df['sentence'].str.replace('[^\w\s]','')
     
     return df
