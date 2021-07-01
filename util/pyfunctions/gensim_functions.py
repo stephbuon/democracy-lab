776b2aae108cf48c04fa8a0ea49822a219f02802
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-07-01 09:59:30 -0500

    Update gensim_functions.py

--- util/pyfunctions/gensim_functions.py
+++ util/pyfunctions/gensim_functions.py
@@ -7,7 +7,7 @@ os.chdir('/users/sbuongiorno/')
 sys.path.append('../democracy-lab/util/pyfunctions/')
 
 from pyfunctions.parallelize_operation import parallelize_operation
-from pyfunctions.str_split_sentences import str_split_sentences
+from pyfunctions.str_functions import str_split_df_sentences
 
 
 def export_gensim_models(dir_path, n_cores):
@@ -22,7 +22,7 @@ def export_gensim_models(dir_path, n_cores):
         
         imported_data = pd.read_csv(dir_path + fname)
         
-        sentences_df = parallelize_operation(imported_data, str_split_sentences, n_cores)
+        sentences_df = parallelize_operation(imported_data, str_split_df_sentences, n_cores)
         
         sentences_df['sentence'] = sentences_df['sentence'].str.split()
         
@@ -43,8 +43,8 @@ def export_gensim_models(dir_path, n_cores):
         save_name = os.path.join(dir_path, fname)
         
         congress_model.save(save_name + '_model')
-
-
+    
+        
 def keyword_context(dir_path, keyword):
     keyword_context = []
     
