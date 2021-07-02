f6f9d5f12adc83610465354ffd52114554cc2d12
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-07-02 11:25:17 -0500

    Update gensim_w2v_functions.py

--- util/pyfunctions/gensim_w2v_functions.py
+++ util/pyfunctions/gensim_w2v_functions.py
@@ -1,13 +1,23 @@
+# export_gensim_w2v_models, w2v_embeddings
 import os
 import sys
 import gensim
 import pandas as pd
 
+# w2v_visualize_scatter_plot
+%matplotlib inline
+from matplotlib import cm
+from numpy import linspace
+from adjustText import adjust_text
+from matplotlib import pyplot as plt
+
+# export_gensim_w2v_models, w2v_embeddings
 os.chdir('/users/sbuongiorno/')
 sys.path.append('../democracy-lab/util/pyfunctions/')
 
 from pyfunctions.parallelize_operation import parallelize_operation
 from pyfunctions.str_functions import str_split_df_sentences, lemmatize_df_text
+
 
 def export_gensim_w2v_models(dir_path, n_cores):
     file_names = []
@@ -82,3 +92,80 @@ class w2v_embeddings:
         return keyword_dates
     
     
+class w2v_visualize_scatter_plot:
+    
+    def define_periods(start, end, interval):
+        periods = range(start, end, interval)
+    
+        period_names = []
+        for period in periods:
+            period_names.append(str(period) + '.0')
+    
+    return period_names
+
+
+    def collect_text_values(keyword_context, period_names):
+        period_words = []
+    
+        for i in range(0, len(period_names)):
+            try:
+                words = [value[0] for value in keyword_context[i]]
+                period_words.append(words)
+            except:
+                continue
+                
+        return period_words
+        
+        
+    def make_1D_list(period_words):
+        flat_list = []
+    
+        for list in period_words:
+            for word in list:
+                flat_list.append(word)
+                
+        return flat_list
+    
+    
+    def w2v_scatter_plot(period_names, keyword_context, flat_list): 
+        
+        colors = [ cm.gnuplot(x) for x in linspace(0, 1, len(flat_list)) ]
+        
+        plt.figure(figsize=(30,30), dpi = 400)
+        
+        texts = []
+        for i in range(0,len(period_names)): # cycle through period names                     
+            for j in range(10): # cycle through first ten words
+                if keyword_context[i]:
+                    xx = period_names[i] # on the x axis, plot the period name
+                    yy = [item[1] for item in keyword_context[i]][j] # on y, plot distance (how close the word is related to the keyword)
+            txt = [item[0] for item in keyword_context[i]][j] # grab the name of each collocated word
+            colorindex = flat_list.index(txt) # keep all dots for the same word the same color
+        
+            plt.scatter(
+                xx, #x axis
+                yy, # y axis
+                linewidth=1, 
+                color = colors[colorindex],
+                s = 300, # dot size
+                alpha=0.5)  # dot transparency
+
+            texts.append(plt.text(xx, yy, txt)) # make a label for each word
+            
+        adjust_text(texts, force_points=0.0001, force_text=0.0035, # helps with overlapping labels -- may take a minute to run
+                    expand_points=(1, 1), expand_text=(1, 1),
+                    arrowprops=dict(arrowstyle="-", color='black', lw=0.5))
+
+
+        plt.xticks(rotation=90)
+
+
+        plt.title("What words were most associated with ''" + keyword + "' in the Hansard debates?", 
+                  fontsize=20, fontweight=0, color='Red')
+        plt.xlabel("period")
+        plt.ylabel("similarity to " + keyword)
+
+        plt.savefig('w2v_' + keyword + '.pdf' )#plt.savefig(keyword + '-over-time-' + str(startdate) + '-' + str(enddate) + '.pdf')
+        plt.show()
+        
+        
