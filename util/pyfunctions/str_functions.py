85239a79b0960aaef53eeb0b766a8f58dafba98f
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-07-02 10:15:00 -0500

    Update str_functions.py

--- util/pyfunctions/str_functions.py
+++ util/pyfunctions/str_functions.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import re
-from textblob import Word
+
+import spacy
+nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+
 
 def str_split_df_sentences(df):
     split_rule = r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s"
@@ -11,17 +14,23 @@ def str_split_df_sentences(df):
     df.reset_index()
     df.rename(columns = {'speech': 'sentence'}, inplace = True)
     
-    #if remove_punctuation == True:
-    df['sentence'] = df['sentence'].str.replace('[^\w\s]','')
+    df['sentence'] = df['sentence'].str.replace('[^\w\s]','') #if remove_punctuation == True:
     
     return df
 
 
-def lemmatize_df_text(df):
-    df['sentence'] = df['sentence'].str.split()
-    df['sentence'] = df['sentence'].apply(lambda x: [Word(word) for word in x])
-    df['sentence'] = df['sentence'].apply(lambda x: [word.lemmatize() for word in x])
-    df['sentence'] = df['sentence'].apply(lambda x: ' '.join(word for word in x))
+#def lemmatize_df_text(df): # from textblob import Word
+#    df['sentence'] = df['sentence'].str.split()
+#    df['sentence'] = df['sentence'].apply(lambda x: [Word(word) for word in x])
+#    df['sentence'] = df['sentence'].apply(lambda x: [word.lemmatize() for word in x])
+#    df['sentence'] = df['sentence'].apply(lambda x: ' '.join(word for word in x))
+    
+#    return(df)
+
+def lemmatize_df_text(df):    
+    #df['sentence'] = list(nlp.pipe(df['sentence']) # instead of this i do nlp(x)
+    df['sentence'] = df['sentence'].apply(lambda x: [token.lemma_ for token in nlp(x) if 'PRON' not in token.lemma_])
+    df['sentence'] = df['sentence'].apply(lambda x: ' '.join(token for token in x))
     
     return(df)
 
