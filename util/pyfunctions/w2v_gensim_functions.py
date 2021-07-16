3e71a5d67961ece4d5d2602223fcec88e09c5338
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-07-16 12:30:58 -0500

    Update w2v_gensim_functions.py

--- util/pyfunctions/w2v_gensim_functions.py
+++ util/pyfunctions/w2v_gensim_functions.py
@@ -19,6 +19,14 @@ from pyfunctions.str_functions import str_split_df_sentences, lemmatize_df_text
 
 
 def w2v_export_gensim_models(dir_path, n_cores):
+    cores_count = os.cpu_count()
+    
+    if cores_count != n_cores:
+        print('Warning: The number of cores requested does not match the number of cores available.')
+        print('Number of cores requested: ' + n_cores)
+        print('Number of cores available: ' + cores_count)
+        print('Defaulting to using ' + cores_count + ' cores.')
+    
     file_names = []
     cycle = 0
     
