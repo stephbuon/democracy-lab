c06d18db8aa123eef7b6366a4660234102396133
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-07-19 13:17:48 -0500

    Create dict_functions.py

---
+++ util/pyfunctions/dict_functions.py
@@ -0,0 +1,14 @@
+def dict_keyword_lookup(dic, keywords_list):
+    filtered_dic = {}
+    
+    regex = re.compile('|'.join(keywords_list))
+    
+    for key, value in dic.items():
+        for item in value:
+            if regex.search(item):
+                if key in filtered_dic:
+                    filtered_dic[key].append(item)
+                else:
+                    filtered_dic[key] = [item]
+    
+    return filtered_dic
