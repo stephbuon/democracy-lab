efc9065a1d54177057526b9161c862826a8c8c68
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-07-19 14:30:07 -0500

    Create sentiment_functions.py

---
+++ util/pyfunctions/sentiment_functions.py
@@ -0,0 +1,18 @@
+def afinn_sentiment(text):
+    return Afinn().score(text)
+
+
+def textblob_sentiment(text):
+    return TextBlob(text).sentiment.polarity
+
+
+def vader_sentiment(text):
+    return SentimentIntensityAnalyzer().polarity_scores(text)
+
+  
+def sentiment_score_df(df, col_name):
+    df['afinn'] = df[col_name].apply(afinn_sentiment)
+    df['textblob'] = df[col_name].apply(textblob_sentiment)
+    df['vader'] = df[col_name].apply(vader_sentiment)
+    df['vader'] = df['vader'].apply(lambda score_dict: score_dict['compound'])    
+    return df
