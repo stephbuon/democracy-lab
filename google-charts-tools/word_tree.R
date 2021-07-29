7fc0c627186d5b91d2984dda9327d765f28c1c49
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-07-29 15:54:33 -0500

    Update word_tree.R

--- google-charts-tools/word_tree.R
+++ google-charts-tools/word_tree.R
@@ -7,7 +7,6 @@ library(googleVis)
 library(tidytext)
 
 #triples <- read_csv("~/posextractr/c19_hansard_debate_text_triples_07232021.csv")
-triples <- read_csv("~/Downloads/landlord_have_power.csv")
 
 #triples_out <- triples %>%
 #  filter(str_detect(triple, "landlord-have-power"))
@@ -17,67 +16,52 @@ triples <- read_csv("~/Downloads/landlord_have_power.csv")
 #triples_out <- triples_out %>%
 #  mutate(decade = year - year %% decade, text)
 
-triples_date <- triples %>%
-  filter(decade == 1890) %>%
-  select(text)
+triples <- read_csv("~/Downloads/landlord_have_power.csv")
 
-j = 25 # how many words constitute a phrase?
+phrase_lengths <- c(10, 25) 
+regexes <- c("^landlord", "landlord$", "^power", "power$")
+decades <- c("1800", "1810", "1820", "1830", "1840", "1850", "1860", "1870", "1880", "1890", "1900", "1910")
 
-out <- triples_date %>%
-  unnest_tokens(text, text, token = "ngrams", n = j)
-
-out_2 <- out %>%
-  filter(str_detect(text, "^landlord"))
-
-wt2 <- gvisWordTree(out_2, textvar = "text",
-                    options = list(fontName = "Times-Roman",
-                                   maxFontSize = 10,
-                                   wordtree = "{word: 'power'}",
-                                   width = 700))
-
-plot(wt2)
-
-
-
-out_3 <- out %>% 
-  filter(str_detect(text, "power$"))
-
-
-wt3 <- gvisWordTree(out_3, textvar = "text",
-                    options = list(fontName = "Times-Roman",
-                                   maxFontSize = 10,
-                                   wordtree = "{word: 'landlord'}",
-                                   width = 700))
-
-plot(wt3)
-
-
-
-out_3 <- out %>% 
-  filter(str_detect(text, "^power"))
-
-
-wt3 <- gvisWordTree(out_3, textvar = "text",
-                    options = list(fontName = "Times-Roman",
-                                   maxFontSize = 10,
-                                   wordtree = "{word: 'power'}",
-                                   width = 700))
-
-plot(wt3)
+for (a in 1:length(phrase_lengths)) {
+  p <- phrase_lengths[a]
+  
+  for (i in 1:length(decades)) {
+    d <- decades[i]
+    
+    filtered <- triples %>%
+      filter(decade == d) %>%
+      select(text)
+    
+    filtered <- filtered %>%
+      unnest_tokens(text, text, token = "ngrams", n = p)
+    
+    for (b in 1:length(regexes)) {
+      r <- regexes[b]
+      
+      filtered_2 <- filtered %>%
+        filter(str_detect(text, r))
+      print(filtered_2)
+      
+      wt2 <- gvisWordTree(filtered_2, 
+                          textvar = "text",
+                          options = list(fontName = "Times-Roman",
+                                         maxFontSize = 10,
+                                         wordtree = "{word: 'power'}",
+                                         width = 700)) 
+      
+      if (NROW(filtered_2)!=0) {
+        plot(wt2) } 
+      
+      wt2 <- gvisWordTree(filtered_2, 
+                          textvar = "text",
+                          options = list(fontName = "Times-Roman",
+                                         maxFontSize = 10,
+                                         wordtree = "{word: 'landlord'}",
+                                         width = 700)) 
+      
+      if (NROW(filtered_2)!=0) {
+        plot(wt2) } } } }
 
 
 
-
-
-out_3 <- out %>% 
-  filter(str_detect(text, "^landlord"))
-
-
-wt3 <- gvisWordTree(out_3, textvar = "text",
-                    options = list(fontName = "Times-Roman",
-                                   maxFontSize = 10,
-                                   wordtree = "{word: 'landlord'}",
-                                   width = 700))
-
-plot(wt3)
 
