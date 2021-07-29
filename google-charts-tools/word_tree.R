7b41865f393c21234e43be9b6c1f59417c3bbae4
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-07-29 00:17:03 -0500

    Update word_tree.R

--- google-charts-tools/word_tree.R
+++ google-charts-tools/word_tree.R
@@ -21,18 +21,63 @@ triples_date <- triples %>%
   filter(decade == 1890) %>%
   select(text)
 
-j = 5 # how many words constitute a phrase?
+j = 25 # how many words constitute a phrase?
 
 out <- triples_date %>%
   unnest_tokens(text, text, token = "ngrams", n = j)
 
-out <- out %>%
+out_2 <- out %>%
+  filter(str_detect(text, "^landlord"))
+
+wt2 <- gvisWordTree(out_2, textvar = "text",
+                    options = list(fontName = "Times-Roman",
+                                   maxFontSize = 10,
+                                   wordtree = "{word: 'power'}",
+                                   width = 700))
+
+plot(wt2)
+
+
+
+out_3 <- out %>% 
   filter(str_detect(text, "power$"))
 
-#wt1 <- gvisWordTree(out, textvar = "text")
-#plot(wt1)
 
-wt2 <- gvisWordTree(out, textvar = "text",
+wt3 <- gvisWordTree(out_3, textvar = "text",
                     options = list(fontName = "Times-Roman",
-                                   wordtree = "{word: 'landlord'}"))
-plot(wt2)
+                                   maxFontSize = 10,
+                                   wordtree = "{word: 'landlord'}",
+                                   width = 700))
+
+plot(wt3)
+
+
+
+out_3 <- out %>% 
+  filter(str_detect(text, "^power"))
+
+
+wt3 <- gvisWordTree(out_3, textvar = "text",
+                    options = list(fontName = "Times-Roman",
+                                   maxFontSize = 10,
+                                   wordtree = "{word: 'power'}",
+                                   width = 700))
+
+plot(wt3)
+
+
+
+
+
+out_3 <- out %>% 
+  filter(str_detect(text, "^landlord"))
+
+
+wt3 <- gvisWordTree(out_3, textvar = "text",
+                    options = list(fontName = "Times-Roman",
+                                   maxFontSize = 10,
+                                   wordtree = "{word: 'landlord'}",
+                                   width = 700))
+
+plot(wt3)
+
