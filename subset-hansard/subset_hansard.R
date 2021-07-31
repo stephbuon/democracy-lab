b89fe4babdf7065de81e424e41257cc3931f00bf
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-07-31 16:15:29 -0500

    Update subset_hansard.R

--- subset-hansard/subset_hansard.R
+++ subset-hansard/subset_hansard.R
@@ -5,6 +5,7 @@ subset_hansard <- function(data, first_year, last_year, export_fpath) {
 
   hansard <- read_tsv(data)
 
+  # i need to fix this -- greater/less than or equal to doesn't wokr -- I also need to change speechdate to a diff col name
   hansard <- hansard %>%
     filter(year(speechdate) >= first_year) %>%
     filter(year(speechdate) =< last_year)
