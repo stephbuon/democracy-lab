570459a1836d450cd342159e94eac9928166e6ee
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-08-10 14:50:10 -0500

    Update preprocess_congress_data_3.R

--- triples/congress-triples/preprocess_congress_data_3.R
+++ triples/congress-triples/preprocess_congress_data_3.R
@@ -12,4 +12,4 @@ ids <- paste0("rid_", ids)
 congress_data <- congress_data %>%
    mutate(row_id = ids)
 
-write_csv("/scratch/group/pract-txt-mine/posextractr/clean_stanford_congressional_records_w_year_2.csv")
+write_csv(congress_data, "/scratch/group/pract-txt-mine/posextractr/clean_stanford_congressional_records_w_year_2.csv")
