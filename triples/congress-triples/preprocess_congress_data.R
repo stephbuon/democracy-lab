908b5106cb7b00d471f982a7528102bb2a9142a6
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-08-10 08:23:35 -0500

    Update preprocess_congress_data.R

--- triples/congress-triples/preprocess_congress_data.R
+++ triples/congress-triples/preprocess_congress_data.R
@@ -3,7 +3,13 @@ library(tidyverse)
 congress_data <- read_csv("/scratch/group/pract-txt-mine/posextractr/clean_stanford_congressional_records_w_year.csv")
 
 congress_data <- congress_data %>%
-  select(-chamber, -number_within_file, -first_name, -last_name, -state, -gender, -line_start, -line_end, -char_count, -word_count) %>%
-  mutate(row_id = row_number())
+  select(-chamber, -number_within_file, -first_name, -last_name, -state, -gender, -line_start, -line_end, -char_count, -word_count)
+
+ids <- 1:186642802
+
+ids <- paste0("rid_", ids)
+
+congress_data <- congress_data %>%
+   mutate(row_id = ids)
 
 write_csv("/scratch/group/pract-txt-mine/posextractr/clean_stanford_congressional_records_w_year_2.csv")
