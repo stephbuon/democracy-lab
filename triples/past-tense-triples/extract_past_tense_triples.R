8351a3e8b7adf57d16ce828f98fea968ac73cf49
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-08-18 19:48:34 -0500

    Create extract_past_tense_triples.R

---
+++ triples/past-tense-triples/extract_past_tense_triples.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+
+triples <- read_csv("~/c19_hansard_debate_text_triples_07232021.csv")
+
+triples$triple <- triples$triple %>%
+  tolower()
+
+# since I do not yet have negation before objects I only go up to six possibilities
+matches <- c(".*-.*ed-.*", ".*-.*-.*ed-.*", ".*-.*-.*-.*ed-.*", ".*-.*-.*-.*ed-.*") 
+
+past_tense_triples <- tibble()
+
+for(i in 1:length(keywords_value)) {
+  
+  match <- matches[i]
+  
+  filtered_triples <- triples %>%
+    filter(str_detect(triple, match))
+  
+  past_tense_triples <- bind_rows(past_tense_triples, filtered_triples) }
+
+#TODO: 
+# went -- look up list of past tense words without -ed 
+# add these irregular verbs: https://www.ecenglish.com/learnenglish/lessons/irregular-verb-list
+
+write_csv(past_tense_triples, "~/past_tense_triples_08192021.csv")
