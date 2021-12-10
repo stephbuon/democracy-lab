e59e220b652b3ee62742ec91f6a33bc4d457da84
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-12-10 17:13:18 -0600

    Update reorder_cols.R

--- util/rfunctions/reorder_cols.R
+++ util/rfunctions/reorder_cols.R
@@ -1,3 +1,5 @@
+# if you have already created a bunch of data sets and the columsn are in the wrong order
+
 library(data.table)
 library(dplyr)
 
@@ -12,7 +14,7 @@ reorder_cols <- function(input_dir) {
     data <- data %>%
       select(sentence_id, token, speechdate)
     
-    export_name <- paste0(input_dir, "/", file)
-    fwrite(export_name) } }
+    export <- paste0(input_dir, "/", file)
+    fwrite(data, export) } }
 
 reorder_cols("/home/stephbuon/projects/smu_19th_century_hansard_corpus_with_improved_speaker_names/hansard_tokens/")
