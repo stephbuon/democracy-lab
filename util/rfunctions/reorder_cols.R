414de47660f803ee34bbb6efcee651c10c359de8
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-12-10 17:11:55 -0600

    Update reorder_cols.R

--- util/rfunctions/reorder_cols.R
+++ util/rfunctions/reorder_cols.R
@@ -12,7 +12,7 @@ reorder_cols <- function(input_dir) {
     data <- data %>%
       select(sentence_id, token, speechdate)
     
-    export_name <- paste0(input_path, "/", file)
+    export_name <- paste0(input_dir, "/", file)
     fwrite(export_name) } }
 
 reorder_cols("/home/stephbuon/projects/smu_19th_century_hansard_corpus_with_improved_speaker_names/hansard_tokens/")
