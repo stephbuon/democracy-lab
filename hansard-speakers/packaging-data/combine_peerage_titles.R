8a7fe05669dc86c54b20bd43c107f3a5efb8f2b5
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2022-06-21 17:05:16 -0500

    Update combine_peerage_titles.R

--- hansard-speakers/packaging-data/combine_peerage_titles.R
+++ hansard-speakers/packaging-data/combine_peerage_titles.R
@@ -1,14 +1,20 @@
 library(tidyverse)
-library(readxl)
 
-dir <- "~/Downloads/peerage-titles/"
-main <- read_csv("~/Downloads/short_list_peerage_titles.csv") 
-files <- list.files(path=dir, pattern="*.xlsx")
+dir <- "~/Downloads/redo/"
+
+main <- read_csv("~/Downloads/redo/name_aliases.csv") %>%
+  select(corresponding_id, alias, real_name, start, end)
+main_2 <- read_csv("~/Downloads/redo/etc_oneoffs.csv") %>%
+  select(corresponding_id, alias, real_name, start, end)
+
+main <- bind_rows(main, main_2)
+
+files <- list.files(path=dir, pattern="*.csv")
 
 for(file in files) {
-  temp <- read_excel(paste0(dir, file)) 
+  temp <- read_csv(paste0(dir, file)) %>%
+    select(corresponding_id,	alias, real_name, start, end)
   
-  temp$corresponding_id <- as.character(temp$corresponding_id)
   temp$start <- as.character(temp$start)
   temp$end <- as.character(temp$end)
   
@@ -19,6 +25,8 @@ main <- main %>%
 
 main$alias <- str_to_title(main$alias)
 main$real_name <- str_to_title(main$real_name)
+
+main <- unique(main)
 
 main <- main %>%
   arrange(alias)
