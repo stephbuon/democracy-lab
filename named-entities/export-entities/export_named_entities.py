7e54bcf830353a57e2686968365655a7125e5b75
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2022-09-13 09:16:16 -0400

    Update export_named_entities.py

--- named-entities/export-entities/export_named_entities.py
+++ named-entities/export-entities/export_named_entities.py
@@ -6,21 +6,21 @@ import spacy
 nlp = spacy.load('en_core_web_sm', disable=['parser', 'lemmatizer', 'tagger'])
 
 def extract_named_entity(sentence):
-    return [str(entity.text) for entity in sentence.ents]
-    # If I want to return tuples (entity.text, entity.label) instead:
-    # return [(entity.text, entity.label_) for entity in sentence.ents]   
+    return [str(entity.text) for entity in sentence.ents] 
     
 def extract_named_entity_category(sentence):
     return [str(entity.label_) for entity in sentence.ents]
 
-def export_named_entities(data, arg1):
+def extract_named_entity_w_category(sentence):
+    return [(entity.text, entity.label_) for entity in sentence.ents]
 
+def remove_rows_no_entities(data):
+    return data[data['named_entities'].map(bool)]
+
+def export_named_entities(data, arg1):
     data['sentence'] = data['sentence'].astype(str)
     data['parsed_text'] = list(nlp.pipe(data['sentence']))
     data['named_entities'] = data['parsed_text'].apply(extract_named_entity)
-    #data['named_entity_category'] = data['parsed_text'].apply(extract_named_entity_category)
-
-    #data = data[data['named_entities'].map(bool)]
 
     if not data.empty:
         handle = open(os.path.basename(arg1) + '.pickle', 'wb')
