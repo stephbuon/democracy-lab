9b80938c54b565d6c3887dace061353edfa17692
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2022-09-22 17:33:57 -0500

    Create count_adj_noun_pairs.R

---
+++ util/DAOTM/count_adj_noun_pairs.R
@@ -0,0 +1,70 @@
+
+# if you do not already have posextractr installed, you will need to install it:
+# require(devtools)
+# install_github("stephbuon/posextractr")
+
+library(data.table)
+library(tidyverse)
+library(posextractr)
+library(reticulate)
+
+posextract_initialize()
+
+hansard <- fread("/home/stephbuon/data/hansard_justnine_w_year.csv") 
+
+hansard <- hansard %>% 
+  select(sentence_id, text, year) %>% 
+  filter(str_detect(text, regex("woman|women", ignore_case = T))) %>% 
+  mutate(decade = year - year %% 10) %>% 
+  select(-year)
+
+decades <- c(1800, 1810, 1820, 1830, 1840, 1850, 1860, 1870, 1880, 1890, 1900)
+
+out <- data.frame()
+
+for(d in decades) {
+  
+  filtered_hansard <- hansard %>%
+    filter(decade == d)
+  
+  adjective_noun_pairs <- extract_adj_noun_pairs(filtered_hansard$text)
+  
+  adjective_noun_pairs <- adjective_noun_pairs %>%
+    mutate(adj_noun_pair = paste(adjective, noun))
+  
+  adjective_noun_pairs$adj_noun_pair <- str_to_lower(adjective_noun_pairs$adj_noun_pair)  
+  
+  adjective_noun_pairs$adj_noun_pair <- str_replace(adjective_noun_pairs$adj_noun_pair, "women", "woman")
+
+  adjective_noun_pairs <- adjective_noun_pairs %>% 
+    filter(str_detect(adj_noun_pair, "ignorant(.*)woman$"))
+
+  adjective_noun_pairs <- adjective_noun_pairs %>% 
+    count(adj_noun_pair) %>%
+    mutate(decade = paste(d))
+  
+  out <- bind_rows(out, adjective_noun_pairs)
+  }
+
+ggplot(out, 
+       aes(x = decade, y = n, group = 1)) +
+  geom_line() + 
+  ggtitle("Count of the Adjective \"Ignorant\" Modifying Lemma \"Woman\"") 
+
+ggsave("plot.png", dpi=700)
+
+ggplot(out, 
+       aes(x = decade, y = n, group = 1)) +
+  geom_line() + 
+  geom_point() +
+  ggtitle("Count of the Adjective \"Ignorant\" Modifying Lemma \"Woman\"") 
+
+ggsave("plot_w_dots.png", dpi=700)
+
+ggplot(out, 
+       aes(x = decade, y = n, group = 1)) +
+  geom_point() +
+  geom_smooth() +
+  ggtitle("Count of the Adjective \"Ignorant\" Modifying Lemma \"Woman\"") 
+
+ggsave("plot_w_smooth.png", dpi=700)
